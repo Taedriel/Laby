e62b154eaf143f9c3a9c49829af222def4bfc71c
Isam <isam.fallengalaxypandora@gmail.com>
2019-10-08 09:55:53 +0200

    fin cpurs brutus

--- src/Moteur/Moteur.py
+++ src/Moteur/Moteur.py
@@ -1,14 +1,24 @@
-from tkinter import Tk as Tk
+from tkinter import *
 
 
 class Moteur():
     """inserer de la doc ici"""
 
-    def __init__(self):
+    def __init__(self, grid=None):
         self.main = Tk()
         self.main.title("MoteurGraphique")
         self.main.geometry(("500x500"))
         self.main.resizable(False, False)
+
+        self.elements = {}
+
+        self.screen = Canvas(self.main, confine=True)
+        self.screen.pack()
+
+        if grid != None and type(grid) == type([]) and type(grid[0]) == type([]):
+            self.grid = grid
+        else:
+            raise(TypeError)
 
     def run(self):
         self.main.mainloop()
