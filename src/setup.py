300d6c9d5dece7cc0f7ce7130119106c486ec18b
Isam <isam.fallengalaxypandora@gmail.com>
2019-10-08 08:04:08 +0200

    .gitignore + changement de branche

--- src/setup.py
+++ src/setup.py
@@ -4,6 +4,6 @@ setup(
     name="LabyrintheAuto",
     version="1.0",
     description="génération de labyrinthe aléatoire de taille variable",
-    executables=[Executable("Labyrinthe.py")],
-    options={'build_exe': {'include_files': ["Case.py", "Traducteur.py"]}},
+    executables=[Executable("Main.py")],
+    options={'build_exe': {'include_files': ["Labyrinthe.py", "Case.py", "Traducteur.py", "CONST.py", "Pathfinder.py"]}},
 )
